f694832db58e66186ef21e73bb20cfc65d5b6e10
KristanPanton <43457073+KristanPanton@users.noreply.github.com>
2025-01-26 18:51:57 -0500

    Game of life always in view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import GameOfLife from "./components/GameOfLife";
+
 export default function App() {
   return (
-    <div className="overflow-x-hidden">
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+    <div className="overflow-x-hidden relative">
+      <GameOfLife />
+      <div className="relative z-10">
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 }
